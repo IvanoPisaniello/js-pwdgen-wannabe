4a1bbffca5e6905f9bb84363f48d0593c64fc3cb
IvanoPisaniello <ivanop92@gmail.com>
2023-06-29 18:18:32 +0200

    added bonus

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,7 @@
 const firstName = prompt("Inserisci il Tuo Nome");
 const lastName = prompt("inserisci il tuo Cognome");
 const favColour = prompt("inserisci il tuo colore preferito");
+const age = prompt("inserisci la tua età")
 
 document.getElementById("name").innerHTML = `Benvenuto: ${firstName} ${lastName}`
-document.getElementById("pssw").innerHTML = `Questa è la tua password: ${firstName}${lastName}${favColour}21`;
+document.getElementById("pssw").innerHTML = `Questa è la tua password: ${firstName}${lastName}${favColour}${parseInt(age) + 21}`;
