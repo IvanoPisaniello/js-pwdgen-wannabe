6b597f25792d0a21720514133b190b75331029a7
IvanoPisaniello <ivanop92@gmail.com>
2023-06-29 13:56:13 +0200

    added pssw generator

---
+++ javascript/main.js
@@ -0,0 +1,5 @@
+const firstName = prompt("Inserisci il Tuo Nome");
+const lastName = prompt("inserisci il tuo Cognome");
+const favColour = prompt("inserisci il tuo colore preferito");
+
+document.getElementById("pssw").innerHTML = `Questa è la tua password: ${firstName}${lastName}${favColour}21`;
